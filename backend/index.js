33967c8173c1c79f5b7ea8aad007210684e76132
madicke12 <cissemadicke8@gmail.com>
2023-11-12 18:13:32 +0000

    sw

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json({ limit: "100mb" }));
 app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
